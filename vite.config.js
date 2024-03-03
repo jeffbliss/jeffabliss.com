df802da27850d0bf47e8f0632fa8a7dbb99e9be9
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-03-03 16:24:59 -0500

    Changing to React and Vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import solid from 'solid-start/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import suidPlugin from '@suid/vite-plugin';
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    solid(),
-    solidPlugin(),
-    suidPlugin(),
-    eslint()
-  ],
-  extends: ['eslint:recommended', 'plugin:solid/recommended'],
-  build: {
-    target: 'esnext',
-  },
-});
+  base: '/jeffabliss.com/',
+  plugins: [react()],
+})
